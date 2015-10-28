782da6ed31f33c025d9e53ae94fda78709084389
wolfy1339 <wolfy1339@users.noreply.github.com>
2015-10-27 21:06:50 -0400

    Rewrite
    Taken from stockbt614

--- bot.py
+++ bot.py
@@ -1,60 +1,283 @@
 from __future__ import print_function
 import socket
+import select
+import traceback
+from time import sleep
+import os
 import sys
+import atexit
+import imp
+import hashlib
+import random
 import json
 
+if not os.path.isfile("config.json"):
+    import shutil
+    shutil.copyfile("configtemplate.json", "config.json")
+    print("configtemplate.json copied to config.json")
+
+# Configuration file
 with open("config.json") as config_file:
-    config = json.load(config_file)  # settings file
-# Config Settings
+    config = json.load(config_file)
+# Configuration variables
 server = config["network"]["server"]
 port = config["network"]["port"]
-channel = (",".join(config["channels"]))
-botnick = config["user"]["nick"]
-realname = config["user"]["realname"]
-ident = config["user"]["ident"]
-password = config["user"]["password"]
-username = config["user"]["username"]
-x = (server, port)
+channels = (",".join(config["channels"]))
+errorchannel = config["errorchannel"]
+botNick = config["user"]["nick"]
+botIdent = config["user"]["ident"]
+botRealname = config["user"]["realname"]
+botAccount = config["user"]["username"]
+botPassword = config["user"]["password"]
+NickServ = True
+ownerHostmasks = config["owner"]["hostmask"]
+adminHostmasks = (",".join(config["admin"]["hostmask"]))
+commandChar = config["prefix"]
+encoding = config["encoding"]
+# A string containing python code that is run whenever an error happens
+errorCode = """with socket.socket() as sock:
+    sock.connect(("termbin.com", 9999))
+    sock.send(traceback.format_exc().encode("utf-8", "replace"))
+    SendMessage(errorchannel, "Error: " + sock.recv(1024).decode("utf-8"))"""
 
-irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # defines the socket
-print("Connecting to: " + server + "\r\n")
-irc.connect(x)  # connects to the server
-irc.send("USER {0} {1} blah :{2}\r\n".format(
-        ident, botnick, realname).encode("UTF-8"))  # user authentication
-irc.send("NICK {0}\r\n".format(botnick).encode("UTF-8"))  # sets nick
-if username == "" or password == "":
-    print("Username or Passwoord is empty!")
-    print("If that is not a mistake, please ignore this message")
-else:
-    irc.send("PRIVMSG NICKSERV :identify {0} {1}\r\n".format(
-        username, password).encode("UTF-8"))  # auth
-irc.send("JOIN {0}\r\n".format(channel).encode("UTF-8"))  # join the channel(s)
+#change to True when done
+configured = config["configured"]
 
-while True:  # puts it in an infinite loop
-    # Receive data
-    binary_data = irc.recv(4096)
-    # Decode data from UTF-8
-    data = binary_data.decode("UTF-8", "ignore")
-    # Split data by spaces
-    words = data.split()
-    if words[0] == "PING":
-        # Respond with PONG
-        irc.send("PONG\r\n".encode("UTF-8"))
-    elif words[1] == "PRIVMSG":
-        if " ".join(words[3:]) == ":Hello world!":
-            channel = words[2]
-            # Respond with a message saying "Hello!"
-            irc.send("PRIVMSG {0} :Hello!\r\n".format(channel).encode("UTF-8"))
-        elif words[0][1:] == config["owner"]["hostmask"] and words[3] == "quit":
-            reason = words[4]
-            if reason:
-                irc.send("QUIT :{0}".format([" ".join(words[4:])]).encode("UTF-8"))
-            else:
-                irc.send("QUIT :Quit requested by {0}".format(config["owner"]["nick"]).encode("UTF-8"))
-        elif words[3] == "commands" or words[3] == "list":
-            irc.send("PRIVMSG {0} :NULL")
-    # Print the data
+from common import *
+mods = {}
+for i in os.listdir("mods"):
+    if os.path.isfile(os.path.join("mods", i)) and i[-3:] == ".py" and i[:-3] not in disabledPlugins:
+        try:
+            mods[i[:-3]] = imp.load_source(i[:-3], os.path.join("mods", i))
+        except Exception:
+            pass
+
+def SocketSend(socket, message):
+    socket.send(message.encode('utf-8'))
+
+def Print(message):
+    if encoding != "utf-8":
+        message = message.encode(encoding, errors="replace").decode(encoding)
+    print(message)
+
+def Connect():
+    global irc
+    Print("Connecting to {0}...".format(server))
+    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    irc.connect((server,port))
+    irc.setblocking(0)
+    SocketSend(irc, "USER {0} {1} {2} :{3}\n".format(botIdent, botNick, botNick, botRealname))
+    SocketSend(irc, "NICK {0}\n".format(botNick))
+    if NickServ:
+        SocketSend(irc, "ns identify {0} {1}\n".format(botAccount, botPassword))
+    else:
+        for i in channels:
+            SocketSend(irc, "JOIN {0}\n".format(i))
+
+def ReadPrefs():
     try:
-      print(data)
-    except UnicodeEncodeError:
-      pass
+        with open('logins.txt') as f:
+            for line in f:
+                if len(line.strip()):
+                    cookies = line.split("|")
+                    logins[cookies[0]] = {"username":cookies[1], "portfolio":{}, "cookies":cookies[2].strip()}
+    except OSError:
+        pass
+
+def WritePrefs():
+    try:
+        with open('logins.txt', 'w') as f:
+            for i in logins:
+                f.write("{0}|{1}|{2}\r\n".format(i, logins[i]["username"], logins[i]["cookies"]))
+    except OSError:
+        pass
+atexit.register(WritePrefs)
+
+def PrintError(channel = None):
+    Print("=======ERROR=======\n{0}========END========\n".format(traceback.format_exc()))
+    if channel:
+        if channel[0] != "#":
+            channel = errorchannel
+        SocketSend(irc, "PRIVMSG {0} :Error printed to console\n".format(channel))
+        if errorCode:
+            try:
+                exec(errorCode)
+            except Exception:
+                SocketSend(irc, "PRIVMSG {0} :We heard you like errors, so we put an error in your error handler so you can error while you catch errors\n".format(channel))
+    
+def Interrupt():
+    SocketSend(irc, "QUIT :Keyboard Interrupt\n")
+    irc.close()
+    quit()
+
+def main():
+    socketQueue = b""
+    while True:
+        try:
+            lines = b""
+            ready = select.select([irc], [], [], 1.0)
+            if ready[0]:
+                lines = irc.recv(2040)
+        except Exception: #socket.error, e:   or   socket.timeout, e:
+            PrintError()
+            return
+        else:
+            lines = socketQueue + lines # add on any queue from the last recv
+            linesSplit = lines.splitlines()
+            socketQueue = b""
+            if lines and lines[-1] != ord("\n"):
+                socketQueue = linesSplit.pop()
+            for line in linesSplit:
+                try:
+                    line = line.decode("utf-8", errors="replace")
+                    Print("<-- "+line+"\n")
+                    text = line.split()
+
+                    if len(text) > 0:
+                        #Reply to server pings
+                        if text[0] == "PING":
+                            SocketSend(irc, "PONG {0}\n".format(text[1]))
+                        elif text[0] == "ERROR":
+                            irc.close()
+                            return #try to reconnect
+
+                    if len(text) > 1:
+                        #Only join channel once identified
+                        if text[1] == "396":
+                            for i in channels:
+                                SocketSend(irc, "JOIN {0}\n".format(i))
+                        #Nickname already in use
+                        elif text[1] == "433":
+                            SocketSend(irc, "NICK {0}-\n".format(text[3]))
+                            if NickServ:
+                                SocketSend(irc, "ns identify {0} {1}\n".format(botAccount, botPassword))
+                                SocketSend(irc, "ns ghost {0}\n".format(botNick))
+                                SocketSend(irc, "NICK {0}\n".format(botNick))
+                        elif text[1] == "437":
+                            SocketSend(irc, "NICK {0}-\n".format(text[3]))
+                            if NickServ:
+                                SocketSend(irc, "ns identify {0} {1}\n".format(botAccount, botPassword))
+                                SocketSend(irc, "ns release {0}\n".format(botNick))
+                                SocketSend(irc, "NICK {0}\n".format(botNick))
+
+                    if len(text) > 2:
+                        #Get channel to reply to
+                        if text[1] == "PRIVMSG":
+                            reply = text[2]
+                            if reply == botNick:
+                                reply = text[0].split("!")[0].lstrip(":")
+                        elif text[1] == "NICK" and text[0].split("!")[0][1:] == botNick:
+                            SocketSend(irc, "NICK {0}\n".format(botNick))
+
+                    if len(text) >= 4:
+                        #Parse line in stocks.py
+                        if len(text):
+                            Parse(text)
+
+                    #allow modules to do their own text parsing if needed, outside of raw commands
+                    for mod in mods:
+                         if hasattr(mods[mod], "Parse"):
+                            mods[mod].Parse(line, text)
+                except SystemExit:
+                    SocketSend(irc, "QUIT :i'm a potato\n")
+                    irc.close()
+                    quit()
+                except Exception:
+                    PrintError(errorchannel or channels[0])
+        try:
+            #allow modules to have a "tick" function constantly run, for any updates they need
+            for mod in mods:
+                if hasattr(mods[mod], "AlwaysRun"):
+                    mods[mod].AlwaysRun(channels[0])
+            #TODO: maybe proper rate limiting, but this works for now
+            temp = False
+            if len(messageQueue) > 7:
+                temp = True
+            for i in messageQueue:
+                Print("--> {0}".format(i))
+                SocketSend(irc, i)
+                if temp:
+                    sleep(1)
+            messageQueue[:] = []
+        except Exception:
+            PrintError(errorchannel or channels[0])
+
+def Parse(text):
+    if text[1] == "PRIVMSG":
+        channel = text[2]
+        username = text[0].split("!")[0].lstrip(":")
+        hostmask = text[0].split("!")[1]
+        command = text[3].lower().lstrip(":")
+        if channel == botNick:
+            channel = username
+        #if username == "FeynmanStockBot":
+        #    return
+
+        #some special owner commands that aren't in modules
+        if CheckOwner(text[0]):
+            if command == "{0}reload".format(commandChar):
+                if len(text) <= 4:
+                    SendNotice(username, "No module given")
+                    return
+                mod = text[4]
+                if not os.path.isfile(os.path.join("mods", mod+".py")):
+                    return
+                commands[mod] = []
+                if mod == "stocks":
+                    if "stocks" in mods:
+                        logins = mods["stocks"].logins
+                        history = mods["stocks"].history
+                        watched = mods["stocks"].watched
+                        news = mods["stocks"].news
+                        specialNews = mods["stocks"].specialNews
+                mods[mod] = imp.load_source(mod, os.path.join("mods", mod+".py"))
+                if mod == "stocks":
+                    mods["stocks"].logins = logins
+                    mods["stocks"].history = history
+                    mods["stocks"].watched = watched
+                    mods["stocks"].news = news
+                    mods["stocks"].specialNews = specialNews
+                SendMessage(channel, "Reloaded {0}.py".format(mod))
+                return
+            elif command == "{0}eval".format(commandChar):
+                try:
+                    command = " ".join(text[4:]).replace("\\n", "\n").replace("\\t", "\t")
+                    ret = str(eval(command))
+                except Exception as e:
+                    ret = str(type(e))+":"+str(e)
+                SendMessage(channel, ret)
+                return
+            elif command == "{0}exec".format(commandChar):
+                try:
+                    exec(" ".join(text[4:]))
+                except Exception as e:
+                    SendMessage(channel, str(type(e))+":"+str(e))
+                return
+            elif command == "{0}quit".format(commandChar):
+                quit()
+
+        #actual commands here
+        for mod in commands:
+            for i in commands[mod]:
+                if command == "{0}{1}".format(commandChar, i[0]):
+                    i[1](username, hostmask, channel, text[4:])
+                    return
+
+try:
+    mods["stocks"].GetStockInfo(True)
+except:
+    pass
+ReadPrefs()
+while True:
+    try:
+        Connect()
+        main()
+        sleep(20)
+    except KeyboardInterrupt:
+        Print("Keyboard inturrupt, bot shut down")
+        break
+    except Exception:
+        PrintError()
+        Print("A strange error occured, reconnecting in 10 seconds")
+        sleep(10)
+        pass
---
+++ mods/general.py
@@ -0,0 +1,169 @@
+from common import *
+import math
+import string
+RegisterMod(__name__)
+
+@command("help", minArgs = 1)
+def HelpCmd(username, hostmask, channel, text, account):
+    """<command> Shows help for a command."""
+    for mod in commands:
+        for i in commands[mod]:
+            if i[0] == text[0]:
+                if i[1].__doc__:
+                    SendMessage(channel, "{0}: {1}".format(text[0], i[1].__doc__))
+                else:
+                    SendMessage(channel, "No help text available for {0}.".format(text[0]))
+                return
+    SendNotice(username, "No such command")
+
+@command("list")
+def ListCmd(username, hostmask, channel, text, account):
+    """(no args). Lists all commands."""
+    if len(text):
+        if not text[0] in commands:
+            SendNotice(username, "No such module")
+        else:
+            SendMessage(channel, "Commands: "+", ".join(i[0] for i in commands[text[0]]))
+    else:
+        SendMessage(channel, "Modules: "+", ".join(i for i in commands))
+
+@command("commands")
+def CommandsCmd(username, hostmask, channel, text, account):
+    """(no args). Lists all commands."""
+    cmdlist = []
+    for mod in commands:
+        cmdlist += commands[mod]
+    SendMessage(channel, ", ".join(i[0] for i in cmdlist))
+
+@command("ping")
+def PingCmd(username, hostmask, channel, text, account):
+    """PONG"""
+    SendMessage(channel, "pong")
+
+@command("join", minArgs = 1, owner = True)
+def JoinCmd(username, hostmask, channel, text, account):
+    """(no args). Make the bot join a channel (admin only)."""
+    Send("JOIN {}\n" % text[0])
+
+@command("part", minArgs = 1, owner = True)
+def PartCmd(username, hostmask, channel, text, account):
+    """(no args). Make the bot part a channel (admin only)."""
+    Send("PART {}\n" % text[0])
+
+"""def Parse(raw, text):
+    if text[1] == "324":
+        SendMessage(channels[2], "Channel exists: "+text[3] + "; " + text[4])
+    elif text[1] == "403":
+        SendMessage(channels[2], "No such channel: "+text[3])"""
+
+"""@command("check", minArgs = 1)
+def PartCmd(username, hostmask, channel, text, account):
+    ""secret command.""
+    for i in text:
+        Send("MODE {}\n" % i)"""
+
+@command("msg", minArgs = 2, owner = True)
+def MsgCmd(username, hostmask, channel, text, account):
+    """(msg <channel> <message>). Sends a message to a channel."""
+    SendMessage(text[0], " ".join(text[1:]))
+
+@command("raw", minArgs = 1, owner = True)
+def MsgCmd(username, hostmask, channel, text, account):
+    """(raw <message>). Sends a raw IRC message."""
+    Send(" ".join(text) + "\n")
+
+"""@command("vigenere", minArgs = 2)
+def Viginerecmd(username, hostmask, channel, text, account):
+    ""(<string> <listofkeys>...). moo""
+    for i in text[1:]:
+        (enc, dec) = Viginere(text[0], i)
+        SendMessage(channel, "Encrypted: {}, Decrypted: {}" % (enc, dec))"""
+
+"""@command("vigenere2", minArgs = 2)
+def Viginere2cmd(username, hostmask, channel, text, account):
+    ""(<string> <listofkeys>...). moo""
+    checked = []
+    for i in text[1:]:
+        (enc, dec) = Viginere(text[0], i)
+        checked.append(dec)
+        Send("MODE ####{}\n" % dec)
+    SendMessage(channels[2], "Checking: " + ", ".join(checked))
+
+@command("vigenere3", minArgs = 2)
+def Viginere3cmd(username, hostmask, channel, text, account):
+    ""(<keys> <listofstrings>...). moo""
+    checked = []
+    for i in text[1:]:
+        (enc, dec) = Viginere(i, text[0])
+        checked.append(dec)
+        Send("MODE ####{}\n" % dec)
+    SendMessage(channels[2], "Checking: " + ", ".join(checked))
+    
+alpha = string.lowercase
+def Viginere(incoming, KEY, offset = 1):
+    ## subtract 1 so it works with python's 0's index for lists
+    ## this part actually broke it, and ended up making it a ROT12
+    ## when it should have been a ROT13
+    offset -= 1
+
+    ## generate the Vigenere square shading
+    chart = dict()
+    k = 0
+    for letter in alpha:
+        if k + offset >= len(alpha):
+            ## rotation won't go beyond end of row
+            sanit = k + offset - len(alpha)
+            chart[letter] = alpha[sanit:] + alpha[:sanit]
+        else:
+            ## rotation goes beyond the end of the row
+            chart[letter] = alpha[k + offset:] + alpha[:k + offset]
+        k += 1
+
+    ## display the chart to stdout
+    ordered_rows = chart.keys()
+    ordered_rows.sort()
+    #for x in ordered_rows:
+    #    print x, chart[x]
+    #print ''
+
+    ## pad the key with the length of the incoming text
+    multi = math.ceil(len(incoming) / float(len(KEY)))
+    long_key = KEY * int(multi)
+
+
+    ## encrypted the incoming text
+    j = 0
+    output_encrypted = str()
+    for x in incoming:
+        y = x.lower()
+        if y in chart:
+            row = chart[y]
+            idx = (string.lowercase).find(long_key[j].lower())
+            output_encrypted += chart[y][idx]
+            j += 1
+        else:
+            output_encrypted += y
+
+    ## decrypt the incoming text
+    p = 0
+    output_decrypted = str()
+    for x in incoming:
+        y = x.lower()
+        stop = False
+        before = len(output_decrypted)
+        for letter in chart:
+            row = chart[letter]
+            if not stop and y in chart:
+                idx = (string.lowercase).find(long_key[p].lower())
+                if row[idx] == y:
+                    output_decrypted += letter
+                    p += 1
+                    stop = True
+        after = len(output_decrypted)
+        if before == after:
+            output_decrypted += y
+
+        return (output_encrypted.upper(), output_decrypted.upper())
+    #SendMessage(channels[2], "Encrypted: {}, Decrypted: {}" % (output_encrypted.upper(), output_decrypted.upper()))
+    #print 'Encrypted:', output_encrypted.upper()
+    #print 'Decrypted:', output_decrypted.upper()"""
