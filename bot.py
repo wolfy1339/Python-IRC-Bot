84b2ef376b3fd6be08a0fafd596fcccedbbaecbb
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-09-13 13:35:41 -0400

    Follow PEP8 strictly

--- bot.py
+++ bot.py
@@ -2,27 +2,28 @@ import zirc
 import ssl
 import socket
 
+
 class Bot(zirc.Client):
     def __init__(self):
-        self.connection = zirc.Socket(family=socket.AF_INET6, wrapper=ssl.wrap_socket)
+        self.connection = zirc.Socket(family=socket.AF_INET6,
+                                      wrapper=ssl.wrap_socket)
         self.config = zirc.IRCConfig(host="irc.freenode.net",
-            port=6697,
-            nickname="wolfyzIRCBot",
-            ident="zirc",
-            realname="A zIRC bot",
-            channels=["##wolfy1339"],
-            sasl_user="BigWolfy1339",
-            sasl_pass=""
-        )
+                                     port=6697,
+                                     nickname="wolfyzIRCBot",
+                                     ident="zirc",
+                                     realname="A zIRC bot",
+                                     channels=["##wolfy1339"],
+                                     sasl_user="BigWolfy1339",
+                                     sasl_pass="")
 
         self.connect(self.config)
         self.start()
 
     def on_privmsg(bot, event, irc):
         irc.reply(event, "It works!")
-        #Or alternatively:
-        #irc.privmsg(event.target, "It works!")
-        
+        # Or alternatively:
+        # irc.privmsg(event.target, "It works!")
+
     def on_all(irc, raw):
         print(raw)
 
