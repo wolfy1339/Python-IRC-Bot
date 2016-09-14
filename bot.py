76e5cccc7651dad6b8af7cb47fe51ce5cf464313
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-09-14 00:08:12 -0400

    More commands, improvements and fixes
    Fix invalid command message, so it only shows up when you address the
    bot
    Add more commands
    Make the example commands in the help bold

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import zirc
 import ssl
 import socket
 import os
+import math
 
 
 class Bot(zirc.Client):
@@ -29,7 +30,9 @@ class Bot(zirc.Client):
         if arguments.startswith("?help"):
             args = arguments.split("?help")[1].strip()
             if args == "quit" or args == "?quit":
-                irc.reply(event, "?quit <message>")
+                irc.reply(event, "(\x02quit <text>\x0F) -- Exits the bot with the QUIT message <text>.")
+            elif args == "echo" or args == "?echo":
+                irc.reply(event, "(\x02echo <text>\x0F) -- Returns the arguments given.")
             else:
                 irc.reply(event, "Invalid command {}".format(arguments.split("?help")[1].strip()))
         elif arguments.startswith("?quit"):
@@ -47,9 +50,45 @@ class Bot(zirc.Client):
             irc.part(arguments.split("?join")[1].strip())
         elif arguments.startswith("?ban"):
             irc.ban(event.target, arguments.split("?ban")[1].strip())
+        elif arguments.startswith("?unban"):
+            irc.unban(event.target, arguments.split("?unban")[1].strip())
+        elif arguments.startswith("?op"):
+            irc.op(event.target, arguments.split("?unban")[1].strip())
+        elif arguments.startswith("?deop"):
+            irc.deop(event.target, arguments.split("?unban")[1].strip())
+        elif arguments.startswith("?voice"):
+            irc.voice(event.target, arguments.split("?voice")[1].strip())
+        elif arguments.startswith("?unvoice"):
+            irc.unvoice(event.target, arguments.split("?voice")[1].strip())
         elif arguments.startswith("?echo"):
             irc.reply(event, arguments.split("?echo")[1].strip())
-        else:
+        elif arguments.startswith("?calc"):
+            safe_dict = {
+                "sqrt": math.sqrt,
+                "pow": math.pow,
+                "sin": math.sin,
+                "cos": math.cos,
+                "tan": math.tan,
+                "asin": math.asin,
+                "acos": math.acos,
+                "atan": math.atan,
+                "abs": abs,
+                "log": math.log,
+                "fact": math.factorial,
+                "factorial": math.factorial
+            }
+
+            try:
+                t = arguments.split("?calc")[1].strip()
+                t = t.replace("pi", math.pi)
+                t = t.replace("e", math.e)
+                a = format(eval(t, {"__builtins__": None}, safe_dict), ",d")
+                irc.reply(event, "The answer is: {0}".format(a))
+            except ArithmeticError:
+                irc.reply(event, "\x034Number undefined or too large.")
+            except Exception:
+                irc.reply(event, "\x034Invalid Input")
+        elif arguments.startswith("?"):
             irc.reply(event, "Invalid command {}".format(arguments.strip()))
         print(event.raw)
 
