f9dea08c2d636c9f93f67789bb614e5f026a9636
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-09-19 00:53:16 -0400

    Add default quit message

--- commands.py
+++ commands.py
@@ -110,6 +110,7 @@ def nick(bot, event, irc, args):
 @add_cmd("quit", admin=True)
 def Quit(bot, event, irc, args):
     """(\x02quit <text>\x0F) -- Exits the bot with the QUIT message <text>."""
+    args = "zIRC - https://github.com/itslukej/zirc" if not args else args
     irc.quit(args)
     time.spleep(1)
     os._exit(0)
