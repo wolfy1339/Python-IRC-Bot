fbc02d351ed569040dae0331142e33564d51c7ea
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-11-08 13:31:00 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import utils
 import commands
 import config
 
-logging.setLevel(10)
+logging.setLevel(30)
 
 class botTest(TestCase):
     def __init__(self):
@@ -38,30 +38,31 @@ class botTest(TestCase):
 bot = botTest()
 log = """:user!~user@user/user PRIVMSG #zirc :Hey!
 :user2!~user@user/user2 PRIVMSG #zirc :How are you?
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ban
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?calc
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?config
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?cycle
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ban *!*@*
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unban *!*@*
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?calc 1+1
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?config ignores
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?cycle #zirc
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?deop
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?echo
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?echo moo
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?help
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?host
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?join
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?join ##foo
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?kban
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?kick
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?kick user,user2,user3
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?list
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?log.level
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?list alias
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?log.level info
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?nick
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?op
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?part
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?perms
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ping
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?quit
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?rainbow
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?rainbow mooo
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?reload
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unban
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unvoice
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?version
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?voice
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unvoice
 :user3!~user@user/user3 PRIVMSG zIRC-test :Hello there!"""
 bot.start(log)
