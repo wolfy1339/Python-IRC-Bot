7954f7e972167e0616986b94c5161f3922a00e98
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-11-08 08:51:23 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,21 +5,9 @@ import commands
 import config
 
 class botTest(TestCase):
-    def on_all(event, irc):
-        if event.raw.startswith("ERROR"):
-            log.error(event.arguments)
-        else:
-            log.debug(event.raw)
-
-    def on_ctcp(irc, raw):
-        log.info("Received CTCP reply " + raw)
-
     def on_privmsg(self, event, irc, arguments):
         if " ".join(arguments).startswith(config.commandChar):
             utils.call_command(self, event, irc, arguments)
-
-    def on_send(data):
-        log.debug(data)
 
     def on_kick(self, event, irc):
         nick = event.raw.split(" ")[3]
@@ -37,43 +25,6 @@ class botTest(TestCase):
             log.info("Invited to %s by %s", event.target, hostmask)
             irc.join(event.target)
 
-    # Numeric events
-    def on_nicknameinuse(self, event, irc):
-        log.error("Nick already in use, trying alternative")
-        irc.nick(self.config['nickname'] + "_")
-
-    def on_bannedfromchan(event, irc):
-        s = event.raw.split(" ")
-        channel = s[3]
-        irc.notice("wolfy1339", "Banned from {0}".format(channel))
-        log.warning("Banned from %s", channel)
-
-    def on_endofmotd(event, irc):
-        log.info("Received MOTD from network")
-
-    def on_welcome(event, irc):
-        log.info("Connected to network")
-
-    def on_whoreply(self, event, irc):
-        (host, ident, nick) = event.arguments[1:3] + event.arguments[4]
-        hostmask = "{0}!{1}@{2}".format(nick, ident, host)
-        self.userdb[nick] = {
-            'hostmask': hostmask,
-            'host': host,
-            'account': None
-        }
-
-    def on_whospcrpl(self, event, irc):
-        (ident, host, nick) = event.arguments
-        hostmask = "{0}!{1}@{2}".format(nick, ident, host)
-        self.userdb[nick] = {
-            'hostmask': hostmask,
-            'host': host,
-            'account': None
-        }
-
-    def on_315(self, event, irc):
-        log.info("Received end of WHO reply from network")
 
 bot = botTest()
 log = """:user!~user@user/user PRIVMSG #zirc :Hey!
