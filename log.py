271f5d1c97253dab203aa467881879f98d458b05
wolfy1339 <wolfy1339@users.noreply.github.com>
2016-11-15 13:46:22 -0500

    Fix logging once and for all

--- log.py
+++ log.py
@@ -239,7 +239,7 @@ class Logger(logging.Logger):
         # self.error('Exception string: %s', eStrId)
 
     def _log(self, level, msg, args, exc_info=None, extra=None):
-        msg = msg.replace("%s", "{}").format(*args)
+        msg = msg % tuple(args)
         logging.Logger._log(self, level, msg, (), exc_info=exc_info,
                             extra=extra)
 
