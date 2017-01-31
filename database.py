10cacc544a0b9b802be4b6b80f36e96816cba393
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-01-30 20:16:41 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,19 @@
-def removeEntry(event):
-    try:
-        del self.userdb[event.target][event.source.nick]
-    except KeyError:
-        for i in self.userdb[event.target].values():
-            if i['host'] == event.source.host:
-                del self.userdb[event.target][i['hostmask'].split("!")[0]]
-                break
+class Database(object):
+    def __init__(self, db):
+        self.userdb = {}
 
-def addEntry(channel, nick, hostmask, host, account):
-    self.userdb[channel][nick] = {
-        'hostmask': hostmask,
-        'host': host,
-        'account': account if account != "0" else None
-    }
+    def removeEntry(event):
+        try:
+            del self.userdb[event.target][event.source.nick]
+        except KeyError:
+            for i in self.userdb[event.target].values():
+                if i['host'] == event.source.host:
+                    del self.userdb[event.target][i['hostmask'].split("!")[0]]
+                    break
+
+    def addEntry(channel, nick, hostmask, host, account):
+        self.userdb[channel][nick] = {
+            'hostmask': hostmask,
+            'host': host,
+            'account': account if account != "0" else None
+        }
