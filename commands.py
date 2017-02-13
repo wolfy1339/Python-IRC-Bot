wolfy1339/Python-IRC-Bot

commit 83c69494eb870e87b27841900d4e318870924735

Use list comprehensions instead of filter

commands.py
@@ -18,9 +18,9 @@ def normalizeWhitespace(s, removeNewline=True):
     ends_with_space = (s[-1] in ' \n\t\r')
     if removeNewline:
         newline_re = re.compile('[\r\n]+')
-        s = ' '.join(filter(bool, newline_re.split(s)))
-    s = ' '.join(filter(bool, s.split('\t')))
-    s = ' '.join(filter(bool, s.split(' ')))
+        s = ' '.join([bool(i) for i in newline_re.split(s)])
+    s = ' '.join([bool(i) for i in s.split('\t')])
+    s = ' '.join([bool(i) for i in s.split(' ')])
     if starts_with_space:
         s = ' ' + s
     if ends_with_space:

commit 74e94260fe40a428e9e3170b1f501478e65fc1aa

Fix bug introduced with 83c69494eb870e87b27841900d4e318870924735

commands.py
@@ -18,9 +18,9 @@ def normalizeWhitespace(s, removeNewline=True):
     ends_with_space = (s[-1] in ' \n\t\r')
     if removeNewline:
         newline_re = re.compile('[\r\n]+')
-        s = ' '.join([bool(i) for i in newline_re.split(s)])
-    s = ' '.join([bool(i) for i in s.split('\t')])
-    s = ' '.join([bool(i) for i in s.split(' ')])
+        s = ' '.join([i for i in newline_re.split(s) if bool(i)])
+    s = ' '.join([i for i in s.split('\t') if bool(i)])
+    s = ' '.join([i for i in s.split(' ') if bool(i)])
     if starts_with_space:
         s = ' ' + s
     if ends_with_space: