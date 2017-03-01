2119a977af0498384d4d1aaacc430632ea2a27d7
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-03-01 16:23:10 -0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,10 @@
-import util
+from . import util
+from . import irc
+sysver = "".join(__import__("sys").version.split("\n"))
+gitver = __import__("subprocess").check_output(['git',
+                                                'rev-parse',
+                                                '--short',
+                                                'HEAD']).decode().split()[0]
+version = "A zIRC bot v{0}@{1}, running on Python {2}".format("0.1",
+                                                              gitver,
+                                                              sysver)
