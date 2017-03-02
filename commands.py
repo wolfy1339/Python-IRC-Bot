d2566ec4161027e1f320e3f8fe08e32295a3e954
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-03-02 11:22:15 -0500

    Update commands.py

--- commands.py
+++ commands.py
@@ -274,9 +274,9 @@ def List(bot, event, irc, args):
     else:
         host = event.source.host
         channel = event.target
-        isOwner = utils.util.check_perms(host, channel, owner=True)
-        isAdmin = utils.util.check_perms(host, channel, admin=True)
-        isTrusted = utils.util.check_perms(host, channel, trusted=True)
+        is_owner = utils.util.check_perms(host, channel, owner=True)
+        is_admin = utils.util.check_perms(host, channel, admin=True)
+        is_trusted = utils.util.check_perms(host, channel, trusted=True)
         owner, admin, trusted, users = [], [], [], []
         text = "Commands({0!s}): {1!s}"
         for i in utils.cmd_list:
@@ -288,13 +288,13 @@ def List(bot, event, irc, args):
                 trusted.append(i)
             else:
                 users.append(i)
-        if isOwner:
+        if is_owner:
             cmd_list = owner + admin + trusted + users
             irc.reply(event, text.format('Owner', ", ".join(cmd_list)))
-        elif isAdmin:
+        elif is_admin:
             cmd_list = admin + trusted + users
             irc.reply(event, text.format('Admin', ", ".join(cmd_list)))
-        elif isTrusted:
+        elif is_trusted:
             cmd_list = trusted + users
             irc.reply(event, text.format('Trusted', ", ".join(cmd_list)))
         else:
@@ -333,15 +333,15 @@ def permissions(bot, event, irc, args):
     channel = event.target
     host = event.source.host
 
-    isBot = host.find("/bot/") != -1
-    isBotChannel = channel in config.bots['channels']
+    is_bot = host.find("/bot/") != -1
+    is_bot_chan = channel in config.bots['channels']
     if utils.util.check_perms(host, channel, owner=True):
         perms = 'Owner'
     elif utils.util.check_perms(host, channel, admin=True):
         perms = 'Admin'
     elif utils.util.check_perms(host, channel, trusted=True):
         perms = 'Trusted'
-    elif host in config.bots['hosts'] or (isBotChannel and isBot):
+    elif host in config.bots['hosts'] or (is_bot_chan and is_bot):
         perms = 'Bot'
     else:
         perms = 'User'
