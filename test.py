e5b81abde0ff535a681a9ca1891dc98191d0cc03
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-04-14 16:24:04 -0400

    Add more tests and move things into alphabetical order

--- test.py
+++ test.py
@@ -43,15 +43,20 @@ log = """:user!~user@user/user PRIVMSG #zirc :Hey!
 :bot!~limnoria@botters/wolf1339/bot/bigwolfy1339 NOTICE #zirc :Channel notice
 :bot!~limnoria@botters/wolf1339/bot/bigwolfy1339 NOTICE zIRC-test :Priv notice
 :user2!~user@user/user2 PRIVMSG #zirc :How are you?
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?>> print("test")
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ban *!*@*
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unban *!*@*
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?calc 1+1
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?calc
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?math 1+1
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?config ignores
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?cfg ignores
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?config ignores
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?cycle
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?cycle #zirc
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?deop
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?deop foo
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?echo moo
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?eval print("test")
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?flush
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?flushq
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?help
@@ -62,18 +67,28 @@ log = """:user!~user@user/user PRIVMSG #zirc :Hey!
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ignore botters/wolf 999
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ignore botters/wolf 999 #zirc
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?join ##foo
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?cycle
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?cycle #zirc
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?kban #zirc GTFO! woof
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?kban #zirc GTFO! woof,moo,woo
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?kick user,user2,user3
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?kick user, user2, user3
 :wuser!~wolfy1339@unaffiliated/wolfy PRIVMSG #zirc :?kick user, user2, user3
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?kick #zirc foo
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?leave #zirc
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?leave
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?list
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ls
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?list alias
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ls alias
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?log.level
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?log.level debug
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?log.level info
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?log.level error
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?log.level warning
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?log.level critical
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?log.level exception
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ls
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ls alias
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?math 2**2
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?nick foo
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ninja #zirc foo
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?op
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?part
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?part #zirc
@@ -81,16 +96,22 @@ log = """:user!~user@user/user PRIVMSG #zirc :Hey!
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?ping
 :wolfy1339bot!~wolfy1339@botters/wolfy/bot/mooobot PRIVMSG #zirc :?ping
 :bot!~limnoria@botters/wolf1339/bot/bigwolfy1339 PRIVMSG ##wolfy1339 :?ping
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?py print("test")
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?quit
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?rainbow mooo
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?rejoin
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?rejoin #zirc
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?reload commands
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?remove foo
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?remove #zirc foo,moo,bo
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unban
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unban foo
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unban #zirc foo
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unvoice
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unvoice foo
+:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unvoice #zirc foo
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?version
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?voice
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?unvoice
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?>> print("test")
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?py print("test")
-:wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?eval print("test")
 :wolfy1339!~wolfy1339@botters/wolfy1339 PRIVMSG #zirc :?invalid
 :user3!~user@user/user3 PRIVMSG zIRC-test :Hello there!"""
 
