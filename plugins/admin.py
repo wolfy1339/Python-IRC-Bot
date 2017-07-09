400603e68515a59a249fa82e86682830aa26ca5c
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-07-09 13:49:25 -0400

    Fix reloading
    AttributeError: 'dict' object has no attribute 'reload'

--- plugins/admin.py
+++ plugins/admin.py
@@ -98,7 +98,7 @@ def reload_cmd(bot, event, irc, args):
     elif utils.util.PY3:
         reload = __import__("imp").reload
     elif utils.util.PY2:
-        reload = __builtins__.reload
+        reload = __builtins__["reload"]
 
     if args[0] != "utils.util":
         try:
