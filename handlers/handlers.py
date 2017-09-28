878dbc74adcfb7ba625e462dfa5019967af142ad
wolfy1339 <wolfy1339@users.noreply.github.com>
2017-09-28 10:58:51 -0400

    Start putting handlers in their own modules

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -20,6 +20,7 @@ class Events(object):
         self.bot = bot
         self.config = self.bot.config
         self.userdb = self.bot.userdb
+        self.server = self.bot.server
 
     def on_all(self, event, irc, arguments):
         if event.raw.find("%") == -1:
@@ -159,6 +160,63 @@ class Events(object):
                 log.info("Received private notice from %s", source)
 
     # Numeric events
+    def on_005(self, event, irc):
+    for param in event.arguments[:-1]:
+        if '=' in param:
+            name, value = param.split('=')
+        else:
+            name = param
+            value = ''
+        if value != '':
+            if ',' in value:
+                for param1 in value.split(','):
+                    if ':' in value:
+                        if ':' in param1:
+                            name1, value1 = param1.split(':')
+                        else:
+                            name1 = param1
+                            value1 = ''
+                        if name not in self.server['ISUPPORT']:
+                            self.server['ISUPPORT'][name] = {}
+                        self.server['ISUPPORT'][name][name1] = value1
+                    else:
+                        if name not in self.server['ISUPPORT']:
+                            self.server['ISUPPORT'][name] = []
+                        self.server['ISUPPORT'][name].append(param1)
+            else:
+                if ':' in value:
+                    name1, value1 = value.split(':')
+                    self.server['ISUPPORT'][name] = {}
+                    self.server['ISUPPORT'][name][name1] = value1
+                elif name == 'PREFIX':
+                    count = 0
+                    value = value.split(')')
+                    value[0] = value[0].lstrip('(')
+                    types = re.split(r'^(.*o)(.*h)?(.*)$', value[0])[1:-1]
+                    levels = {
+                        'op': types[0],
+                        'halfop': types[1] or '',
+                        'voice': types[2]
+                    }
+                    self.server['ISUPPORT'][name] = {}
+                    self.server['prefixes'] = {}
+                    for mode in value[0]:
+                        name1 = mode
+                        value1 = value[1][count]
+                        count += 1
+                        for level in levels.items():
+                            if mode in level[1]:
+                                self.server['prefixes'][value1] = {
+                                    'mode': mode,
+                                    'level': level[0]
+                                }
+                                break
+                        self.server['ISUPPORT'][name][name1] = value1
+                else:
+                    self.server['ISUPPORT'][name] = value
+        else:
+            self.server['ISUPPORT'][name] = value
+
     def on_unavailresource(self, event, irc):
         log.error("Nick unavailable, trying alternative")
         irc.nick(self.config['nickname'] + "_")
